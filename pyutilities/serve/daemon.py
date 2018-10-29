a8f3a53ab9c62c8425161516dd0134b6e1b2b2f1
Zhang Wei E <wei.e.zhang@intel.com>
2018-10-29 11:04:45 +0800

    license: correct license clarification
    Signed-off-by: Zhang Wei E <wei.e.zhang@intel.com>

--- pyutilities/serve/daemon.py
+++ pyutilities/serve/daemon.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017 Intel Corporation
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2006 Edgewall Software
+# All rights reserved.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/wiki/TracLicense.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/log/.
 
 import atexit
 import errno
