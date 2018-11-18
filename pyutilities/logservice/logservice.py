bf71de152c1f6643a7657f7d01c7a0512d9ee4f8
xwu99 <xiaochang.wu@intel.com>
2018-11-18 12:52:09 +0800

    Update logservice.py

--- pyutilities/logservice/logservice.py
+++ pyutilities/logservice/logservice.py
@@ -13,10 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# -----------------------------
-# Copyright by Intel
-# -----------------------------
-
 import sys
 import logging
 
