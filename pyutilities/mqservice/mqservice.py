78df43d2914d3ad575d85535146bcc8fae8020ce
xwu99 <xiaochang.wu@intel.com>
2018-11-18 12:52:51 +0800

    Update mqservice.py

--- pyutilities/mqservice/mqservice.py
+++ pyutilities/mqservice/mqservice.py
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# -----------------------------
-# Copyright by Intel
-# -----------------------------
 import json
 import sys
 import time
